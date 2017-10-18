ce7af56f371dd4cee72431fa7291dc6c0b8f7ee1
Charizard11 <baskicisamet@gmail.com>
2017-10-18 16:32:27 +0300

    adding controller

---
+++ src/main/java/com/sam/spring5training/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.sam.spring5training.controller;
+
+import com.sam.spring5training.repository.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+     public String getBooks(Model model){
+
+        model.addAttribute("books",bookRepository.findAll());
+
+        return "books";
+
+     }
+
+}
